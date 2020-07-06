552418beca0358033367ce3d7c1d8ec178594f78
omri wallach <omriwallach1992@gmail.com>
2020-07-06 12:44:20 +0200

    connect graphql to react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {ApolloProvider,Query} from 'react-apollo';
+import {client} from './graphql/apollo.config'
+import { GET_BOATS } from './graphql/GET_BOATS';
 
-function App() {
-  return (
-    <div className="App">
-      react
-    </div>
-  );
-}
-
-export default App;
+export const App = () => <ApolloProvider
+  client={client}
+>
+  <div>
+    <Query query={GET_BOATS}>
+      {
+        ({ loading, error, data }) => {
+          if (!error && !loading) {
+            return data.getBoats.map(boat => <div>{boat.name}</div>)
+          }
+          else
+            return <div>no data</div>
+        }
+      }
+    </Query>
+</div>
+</ApolloProvider>
