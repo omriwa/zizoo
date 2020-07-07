63e73b6054aacefe5521d61e065aec147bd35f33
omri wallach <omriwallach1992@gmail.com>
2020-07-07 11:16:58 +0200

    add router

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,14 +1,13 @@
-import React,{memo} from 'react';
+import React, { memo } from 'react';
 import { GridList } from '@material-ui/core';
 
 export const Gallery = memo(({ children, cellHeight, cols }) => {
-    console.log(children.length)
     return <GridList
         cellHeight={cellHeight}
         cols={cols}
     >
-    {
-        children
-    }
-</GridList>
+        {
+            children
+        }
+    </GridList>
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router,Route,Redirect} from 'react-router-dom'
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from 'react-apollo';
@@ -8,9 +9,26 @@ import { client } from './graphql/apollo.config'
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider
-      
-      client={client}>
-      <App />
+      client={client}
+    >
+      <Router
+        location='search'
+      >
+<Route
+        path='/'
+        exact
+      >
+        <Redirect
+          from='/'
+          to='/search'
+        />
+      </Route>
+        <Route
+          exact
+          path='/search'
+          component={App}
+        />
+        </Router>
       </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
